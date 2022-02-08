6f27534cc97f8f1a32201405b753306f80c4bb6f
Mikael Voss <mvs@nyantec.com>
2022-02-08 14:54:42 +0100

    Drop stabilised feature attributes

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,10 @@
 #![feature(
 	allocator_api,
-	available_parallelism,
 	core_intrinsics,
-	maybe_uninit_extra,
 	maybe_uninit_slice,
 	nonnull_slice_from_raw_parts,
 	slice_ptr_get,
 	slice_ptr_len,
-	vec_spare_capacity,
 )]
 
 pub mod auxiliary;
